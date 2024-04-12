474c51a5af642d4d50b001742a8cb619fd149748
ias2858 <131394537+theonewhoPrints@users.noreply.github.com>
2024-04-12 13:38:40 -0400

    Update MonteCarlo.py

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,2 +1,14 @@
 import numpy as p
 import scipy.stats
+
+"""
+file: MonteCarlo.py
+course: MATH 251 
+Final Project: MonteCarloIntegration
+authors: Isaac Soares, Kaden Shuman, Chang you Yu
+
+Purpose:
+
+"""
+
+
