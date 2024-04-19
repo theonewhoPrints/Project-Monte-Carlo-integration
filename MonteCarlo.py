3b0b6ad61a27c04b103dcdb437a7fe6667bf4c9f
Kaden Shuman <kms7203@rit.edu>
2024-04-19 13:14:30 -0400

    ok

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,4 +1,4 @@
-import numpy as p
+import numpy as np
 import scipy.stats
 
 """
@@ -17,11 +17,11 @@ distrib1 = scipy.stats.uniform(loc=-3,scale=6)
 
 # One dimensional example
 def integrate_me(f, distrib, npts=100):
-x = distrib.rvs(npts)
-ps = distrib.pdf(x)
-f = f(x)
-mu = np.mean(f/ps)
-err = np.std(f/ps)/np.sqrt(npts)
-return mu,err
+    x = distrib.rvs(npts)
+    ps = distrib.pdf(x)
+    f = f(x)
+    mu = np.mean(f/ps)
+    err = np.std(f/ps)/np.sqrt(npts)
+    return mu,err
 
 # def integrate_me(f, distrib, npts=100):
