d04c1fb564d7336629e073b8521faf70eb716bf4
ias2858 <131394537+theonewhoPrints@users.noreply.github.com>
2024-04-25 08:50:02 -0400

    part 2

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -22,22 +22,26 @@ i = 0
 def func1(x):
     return 1
 
+# Vectorized standard normal PDF function
 def stdNormal1(x):
     mean = 0
     std_dev = 1
-    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * math.exp(-0.5 * ((x - mean) / std_dev) ** 2)
+    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * np.exp(-0.5 * ((x - mean) / std_dev) ** 2)
     return pdf
 
+
+# Vectorized standard normal PDF function with mean -3
 def stdNormal2(x):
     mean = -3
     std_dev = 1
-    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * math.exp(-0.5 * ((x - mean) / std_dev) ** 2)
+    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * np.exp(-0.5 * ((x - mean) / std_dev) ** 2)
     return pdf
 
+# Vectorized standard normal PDF function with mean 3 and standard deviation 3
 def stdNormal3(x):
     mean = 3
     std_dev = 3
-    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * math.exp(-0.5 * ((x - mean) / std_dev) ** 2)
+    pdf = (1 / (std_dev * math.sqrt(2 * math.pi))) * np.exp(-0.5 * ((x - mean) / std_dev) ** 2)
     return pdf
 
 def weighted_normal(x):
@@ -56,20 +60,16 @@ def rosenbrock_function(x1, x2):
 def integrate_me(f, distrib, npts):
     x = distrib.rvs(npts)
     ps = distrib.pdf(x)
-    sum = 0
-    for val in x:
-        sum += f(val)
-    #f = f(x)
-    mean = sum/npts
-    f = mean
-    mu = np.mean(f / ps)
-    err = np.std(f / ps) / np.sqrt(npts)
+    f_values = f(x)
+    mu = np.mean(f_values / ps)
+    err = np.std(f_values / ps) / np.sqrt(npts)
     return mu, err
 
-print("(1)The caculated mean and error is: " , integrate_me(func1, scipy.stats.uniform(loc=-10, scale=20), 1000))
+
+print("(1)Integral estimate for f(x) = 1 on [10, 10]:" , integrate_me(func1, scipy.stats.uniform(loc=-10, scale=20), 1000))
 
 for j in range(0, 6):
-    print("(2, i = " + str(i) + " )The caculated mean and error is: " , integrate_me(func2, scipy.stats.uniform(loc=-1, scale=2), 1000))
+    print("(2, i = " + str(i) + " )The caculated mean and error for Legendre polynomial P{}(x) is: " , integrate_me(func2, scipy.stats.uniform(loc=-1, scale=2), 1000))
     i+= 1
 
 print("(3)Integration for standard normal " , integrate_me(stdNormal1, scipy.stats.uniform(loc=-10, scale=20), 1000))
@@ -91,6 +91,22 @@ class MyTwoDUniform(object):
         V = np.prod([self.bounds[:, 1] - self.bounds[:, 0]])
         return np.ones(x.shape[0]) / V
 
+# Define truncated standard normal distribution
+trunc_std_normal = scipy.stats.truncnorm(-3, 3, loc=0, scale=1)
+
+# Function to integrate using truncated standard normal distribution
+def integrate_with_truncated_normal(f, npts):
+    x = trunc_std_normal.rvs(npts)
+    ps = trunc_std_normal.pdf(x)
+    f_values = f(x)
+    mu = np.mean(f_values / ps)
+    err = np.std(f_values / ps) / np.sqrt(npts)
+    return mu, err
+
+# Repeat the calculations for parts (a), (c), and (d) using truncated standard normal distribution
+print("(2a) Integration on [10, 10] with f(x) = 1: ", integrate_with_truncated_normal(lambda x: 1, 1000))
+print("(2c) Integration of standard normal PDF (truncated):", integrate_with_truncated_normal(stdNormal1, 1000))
+print("(2d) Integration of weighted normal distribution: ", integrate_with_truncated_normal(weighted_normal, 1000))
 
 # Example of integration
 my2d = MyTwoDUniform(bounds=[[-5,5],[-5,5]])
